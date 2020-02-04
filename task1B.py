b789547ea25eb6ff176e14443fe9ba8b628a662c
Annabel Ribeiro-Castro <ar2023@cam.ac.uk>
2020-02-04 15:00:45 +0000

    task1B annabel

---
+++ task1B.py
@@ -0,0 +1,26 @@
+# task of finding the stations which are 10 closest and 10 furthest
+p = (52.2053, 0.1218)
+from floodsystem.stationdata import build_station_list
+from haversine import haversine, Unit
+import python_utils
+stations = build_station_list()
+def sorted_by_key(x, i, reverse=False):
+    def key(element):
+        return element[i]
+    return sorted(x, key=key, reverse=reverse)
+
+def stations_by_distance(stations, p):
+    stations = build_station_list()
+    stat_town_dist = []
+    for station in stations:
+        distance = haversine(p, station.coord)
+        stat_town_dist.append((station.name, distance, station.town))
+    ordered = sorted_by_key(stat_town_dist, 1, reverse=False)
+    print("the 10 closest stations are: (", ordered[:10], ")")
+    print("")
+    print("the 10 furthest stations are: (", ordered[-10:], ")")
+
+stations_by_distance(stations, p)
+
+
+
