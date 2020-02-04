94e360b9f9bb03cd9e4269c8a743454625b05317
Joshua Abu <jea68@cam.ac.uk>
2020-02-04 14:50:57 +0000

    Task1F Josh

---
+++ Task1F.py
@@ -0,0 +1,35 @@
+#Function that returns false if the first value is bigger than second + a function that shows all stations which are wrong
+
+from floodsystem.stationdata import build_station_list
+stations = build_station_list()
+
+
+def typical_range_consistent(self):
+    for station in self:
+        typical_range = station.typical_range
+        if typical_range == None:
+            print(False)
+        else:
+            difference = typical_range[0] - typical_range[1]
+            print(difference < 0)
+
+
+def inconsistent_typical_range__stations(stations):
+    y = []
+    true_stations = []
+    for station in stations:
+        typical_range = station.typical_range
+        if typical_range == None:
+            z = ["{}".format(station.name)]
+            y = y + z
+        else:
+            difference = typical_range[0] - typical_range[1]
+            if difference > 0:
+                x = ["{}".format(station.name)]
+                y = y + x
+    
+    false_stations = sorted(y)
+    return false_stations
+
+print(inconsistent_typical_range__stations(stations))
+print(typical_range_consistent(self=stations))
