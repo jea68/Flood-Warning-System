f0e66d754d226b5a62583f3d11af10ff12faa4e4
Annabel Ribeiro-Castro <ar2023@cam.ac.uk>
2020-02-24 15:01:55 +0000

    Task2C complete

--- Task2C.py
+++ Task2C.py
@@ -1,29 +1,18 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.station import MonitoringStation
 stations = build_station_list()
-def relative_water_level(self):
-    update_water_levels(stations)
-    high_low = []
+update_water_levels(stations)
+def stations_highest_rel_level(stations, N):
+    station_level =[]
+    tol = 0.8
     for station in stations:
         if station.latest_level == None:
-            high_low = high_low
-        elif station.latest_level >= 0.75:
-            high_low.append(station)
-        elif station.latest_level <= 0.25:
-            high_low.append(station)
-    stations_level_over_threshold(high_low, 0.8)
-
-def stations_level_over_threshold(stations, tol):
-    station_level =[]
-    for station in stations:
-        if station.latest_level > tol:
-            station_level.append((station.name, station.latest_level))
+            station_level = station_level
+        elif station.latest_level > tol:
+            station_level.append((station.name, station.latest_level))    
     station_level.sort(key=lambda x: float(x[1]), reverse=True)
-    stations_highest_rel_level(station_level, len(station_level))
-
-
-def stations_highest_rel_level(stations, N):
-    highest = stations[0:10]
+    highest = station_level[0:N]
     print(highest)
     return(highest)
-relative_water_level(stations)
+  
+stations_highest_rel_level(stations, 10)
