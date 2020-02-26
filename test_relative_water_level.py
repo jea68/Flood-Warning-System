6cba1852b83bc213c7ed12f06bfc5438ba4b2cd9
Annabel Ribeiro-Castro <ar2023@cam.ac.uk>
2020-02-26 00:11:17 +0000

    test_relative_water_level complete

--- test_relative_water_level.py
+++ test_relative_water_level.py
@@ -6,7 +6,7 @@ stations = build_station_list()
 update_water_levels(stations)
 def test_stations_level_over_threshold():
     output_level = stations_level_over_threshold(stations, 10000)
-    assert output_level == None
-test_stations_level_over_threshold()
+    assert len(output_level) == 0
+
 
     
